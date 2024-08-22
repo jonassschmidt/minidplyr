8be0d2575667084f4cbb3776eb36648553989fd9
jonassschmidt <jonasschmidt@clin.au.dk>
2024-08-22 10:48:49 +0200

    init pkg with fun zelect() and add()

---
+++ R/add.R
@@ -0,0 +1,20 @@
+#' Title
+#' 
+#' This will be the description.
+#' 
+#' These will be the details.
+#' 
+#'
+#' @param x 
+#' @param y 
+#'
+#' @return
+#' @export
+#' 
+#'
+#' @examples
+#' my_fancy_add(1,2)
+#' my_fancy_add(2:3,4:5)
+my_fancy_add <- function(x,y) {
+  x + y
+}
---
+++ R/select.R
@@ -0,0 +1,18 @@
+#' Takes A data.frame And Returns Specified Variables
+#'
+#' @param df data.frame
+#' @param variables vector of column names, indices or booleans,
+#'   bla bla
+#'
+#' @return A  data.frame with the subsetted variables.
+#' @export
+#'
+#' @examples
+#' options(max.print = 10)
+#' zelect(iris, c("Sepal.Length", "Sepal.Width"))
+#' zelect(iris, c(1,2))
+#' zelect(iris, c(FALSE,TRUE,FALSE,TRUE,FALSE))
+#' 
+zelect <- function(df, variables) {
+  df[variables]
+}
